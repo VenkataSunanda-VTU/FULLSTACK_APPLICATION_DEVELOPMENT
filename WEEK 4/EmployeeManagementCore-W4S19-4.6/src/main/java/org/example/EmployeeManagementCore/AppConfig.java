cae32a3dca72ea3d3454ac954a7934e81ec7c67c
Venkata_Sunanda_VTU <vtu26377@veltech.edu.in>
2026-02-26 14:15:17 +0530

    Add files via upload

---
+++ WEEK 4/EmployeeManagementCore-W4S19-4.6/src/main/java/org/example/EmployeeManagementCore/AppConfig.java
@@ -0,0 +1,9 @@
+package org.example.EmployeeManagementCore;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.employee.core")
+public class AppConfig {
+}
