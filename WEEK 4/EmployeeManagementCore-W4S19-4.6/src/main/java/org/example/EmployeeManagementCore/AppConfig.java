6b958bf0acd0c1e04b8c9af29a2345679bf12d8f
Venkata_Sunanda_VTU <vtu26377@veltech.edu.in>
2026-02-26 14:13:23 +0530

    Delete WEEK 4/EmployeeManagementCore-W4S19-4.6 directory

--- WEEK 4/EmployeeManagementCore-W4S19-4.6/src/main/java/org/example/EmployeeManagementCore/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package org.example.EmployeeManagementCore;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "com.employee.core")
-public class AppConfig {
-}
