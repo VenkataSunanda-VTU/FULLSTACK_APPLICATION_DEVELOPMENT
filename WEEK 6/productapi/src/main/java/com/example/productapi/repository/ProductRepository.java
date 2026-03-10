c6c607b375b9191a4bdabe633e800eda01b661d8
Venkata_Sunanda_VTU <vtu26377@veltech.edu.in>
2026-03-10 10:02:32 +0530

    Delete WEEK 6 directory

--- WEEK 6/productapi/src/main/java/com/example/productapi/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.productapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.productapi.model.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
